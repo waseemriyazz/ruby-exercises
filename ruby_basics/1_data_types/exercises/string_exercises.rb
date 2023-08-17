33ce100e57b523e7c5fc40209a6b05610893c4a3
waseemriyazz <waseemriyaz717@gmail.com>
2023-08-17 11:20:59 +0530

    solved 1_data_types

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -5,36 +5,45 @@ end
 
 def concatenate(string)
   # use concatenation to format the result to be "Hello <string>!"
+  "Hello " << string << "!"
 end
 
 def substrings(word)
   # return the first 4 letters from the word using substrings
+  word[0,4]
 end
 
 def capitalize(word)
+  word.capitalize
   # capitalize the first letter of the word
 end
 
 def uppercase(string)
+  string.upcase
   # uppercase all letters in the string
 end
 
 def downcase(string)
+  string.downcase
   # downcase all letters in the string
 end
 
 def empty_string(string)
+  string.empty?
   # return true if the string is empty
 end
 
 def string_length(string)
+  string.length
   # return the length of the string
 end
 
 def reverse(string)
+  string.reverse
   # return the same string, with all of its characters reversed
 end
 
 def space_remover(string)
+  string.gsub(" ","")
   # remove all the spaces in the string using gsub
 end
